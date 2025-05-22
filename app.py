983ec0c7bc64f37df4eb24fd6f9bc2ad24d7001c
Vahed Barzegari <vahed811@gmail.com>
2025-05-21 23:48:25 -0400

    dhashboard styling

--- app.py
+++ app.py
@@ -35,24 +35,15 @@ ui.tags.style(
             justify-content: center;
             background-color: black;
             height: 90px;
+            padding: 0px !important;
+            margin: 0px !important;
         }
 
 
-        .logo-container {
-            margin-right: 5px;
-            height: 100%;
-            padding: 10px;
-        
-        }
-
-        .logo-container img {
-            height: 60px;
-        }
-
-        .title-container h2 {
+        .title-container h3 {
             color: white;
-            padding: 5px;
-            margin: 0;
+            padding: 0px !important;
+            margin: 0px !important;
         
         }
 
@@ -62,22 +53,39 @@ ui.tags.style(
         body {
 
             background-color: #5e41aa;
+            padding: 0px !important;
+            Margin: 0px !important;
         
         
         }
 
-
+        .card {
+            background-color: white;
+            padding: 0px !important;
+            border-radius: 0px;
+            box-shadow: 2px 2px 10px rgba(0, 250, 0, 0.1);
+            margin: 0px !important
+        }
         .modebar{
             display: none;
         
         }
-
+        .info-gen-css {
+            font-size: 16px !important; /* Adjust the size as needed */
+            color: white !important;
+            display: flex !important; /* Enables centering */
+            align-items: center !important; /* Centers vertically */
+            justify-content: center !important; /* Centers horizontally */
+            text-align: center !important; /* Ensures text alignment */
+            font-weight: bold !important; /* Makes the text bold */
+            background-color: purple !important;
+        }
     """
 )
 
 
 
-ui.page_opts(window_title="GTFS DASHBOARD", fillable=False)
+ui.page_opts(window_title="Toronto Collision", fillable=True)
 
 
 
@@ -86,54 +94,56 @@ ui.page_opts(window_title="GTFS DASHBOARD", fillable=False)
 with ui.div(class_="header-container"):
 
     with ui.div(class_="title-container"):
-        ui.h2("Toronto Collision Dashboard")
+        ui.h3("Toronto Collision Dashboard")
 
-############################################
 with ui.card():
-    
-    ui.card_header("General Information")
-    with ui.layout_columns(col_widths={"sm": (4,4,4)}):
+        
+    ############################################
+    with ui.card():
+        
+        ui.card_header("General Information", class_="info-gen-css")
+        with ui.layout_columns(col_widths={"sm": (4,4,4)}):
 
-        with ui.value_box(
-            showcase=faicons.icon_svg("calendar-days", width="50px"),
-            theme="bg-gradient-green-red",
-        ):
-            "Start Date"
+            with ui.value_box(
+                showcase=faicons.icon_svg("calendar-days", width="50px"),
+                theme="bg-gradient-green-red",
+            ):
+                "Start Date"
 
-            @render.ui  
-            def datestartfun():  
-                return "January, 2014"  
-            
+                @render.ui  
+                def datestartfun():  
+                    return "January, 2014"  
+                
 
-        with ui.value_box(
-            showcase=faicons.icon_svg("calendar-days", width="50px"),
-            theme="bg-gradient-orange-red",
-        ):
-            "End Date"
+            with ui.value_box(
+                showcase=faicons.icon_svg("calendar-days", width="50px"),
+                theme="bg-gradient-orange-red",
+            ):
+                "End Date"
 
-            @render.ui  
-            def dateendfun():  
-                return "December, 2024"  
-            
-
-
-        with ui.value_box(
-            showcase=faicons.icon_svg("car-burst", width="50px"),
-            theme="bg-gradient-yellow-purple",
-        ):
-            "Number of Collisions"
-
-            @render.ui
-            def routefun():
-                return len(database)
+                @render.ui  
+                def dateendfun():  
+                    return "December, 2024"  
+                
 
 
-with ui.card(height="500px"):
+            with ui.value_box(
+                showcase=faicons.icon_svg("car-burst", width="50px"),
+                theme="bg-gradient-yellow-purple",
+            ):
+                "Number of Collisions"
 
-    @render.ui
-    def plot_network():
-        m = folium.Map(location=[43.6, -79.4], zoom_start=10)
-        HeatMap(data=database[['LAT_WGS84', 'LONG_WGS84']].values, radius=6).add_to(m)
-        return m
+                @render.ui
+                def routefun():
+                    return len(database)
+
+
+    with ui.card(full_screen=True, height="500px"):
+
+        @render.ui
+        def plot_network():
+            m = folium.Map(location=[43.6, -79.4], zoom_start=10)
+            HeatMap(data=database[['LAT_WGS84', 'LONG_WGS84']].values, radius=6).add_to(m)
+            return m
 
 
